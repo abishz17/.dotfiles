df011c59c7d72a114ba164f8264f45317081c295
abishz17 <075bct008.abish@pcampus.edu.np>
2025-07-09 15:31:26 +0545

    add inline diagnostics

--- .config/nvim/lua/vim-commands.lua
+++ .config/nvim/lua/vim-commands.lua
@@ -17,6 +17,7 @@ vim.keymap.set("n", "<C-h>", "<C-w>h", { silent = true })
 vim.keymap.set("n", "<C-j>", "<C-w>j", { silent = true })
 vim.keymap.set("n", "<C-k>", "<C-w>k", { silent = true })
 vim.keymap.set("n", "<C-l>", "<C-w>l", { silent = true })
+vim.diagnostic.config({ virtual_text = true })
 
 vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
 
