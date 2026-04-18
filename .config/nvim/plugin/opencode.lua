d4ae25aaa8c55f2b7ad410cacce93c8d1c2ff2e0
abishz17 <075bct008.abish@pcampus.edu.np>
2026-04-18 23:33:17 +0545

    add opencode.nvim

---
+++ .config/nvim/plugin/opencode.lua
@@ -0,0 +1,37 @@
+vim.pack.add({ 'https://github.com/nickjvandyke/opencode.nvim' })
+
+vim.g.opencode_opts = {
+  provider = {
+    enabled = "kitty",
+  },
+}
+
+vim.o.autoread = true
+
+vim.keymap.set({ "n", "x" }, "<leader>aa", function()
+  require("opencode").ask("@this: ", { submit = true })
+end, { desc = "Ask opencode" })
+
+vim.keymap.set({ "n", "x" }, "<leader>as", function()
+  require("opencode").select()
+end, { desc = "Opencode select action" })
+
+vim.keymap.set({ "n", "t" }, "<leader>at", function()
+  require("opencode").toggle()
+end, { desc = "Toggle opencode" })
+
+vim.keymap.set({ "n", "x" }, "go", function()
+  return require("opencode").operator("@this ")
+end, { desc = "Add range to opencode", expr = true })
+
+vim.keymap.set("n", "goo", function()
+  return require("opencode").operator("@this ") .. "_"
+end, { desc = "Add line to opencode", expr = true })
+
+vim.keymap.set("n", "<leader>au", function()
+  require("opencode").command("session.half.page.up")
+end, { desc = "Scroll opencode up" })
+
+vim.keymap.set("n", "<leader>ad", function()
+  require("opencode").command("session.half.page.down")
+end, { desc = "Scroll opencode down" })
