4f3cb3dd8f70ed7b48468657948e902a1409ae63
abishz17 <075bct008.abish@pcampus.edu.np>
2026-05-17 17:02:31 +0545

    i wanna rosepine

--- .config/nvim/plugin/theme.lua
+++ .config/nvim/plugin/theme.lua
@@ -1,11 +1,20 @@
+-- vim.pack.add({
+--   { src = 'https://github.com/vague-theme/vague.nvim', name = 'vague' },
+-- })
+--
+-- require("vague").setup({
+--   transparent = false,
+--   bold = true,
+--   italic = true,
+-- })
+--
+-- vim.cmd("colorscheme vague")
+
 vim.pack.add({
-  { src = 'https://github.com/vague-theme/vague.nvim', name = 'vague' },
+	{
+		src = "https://github.com/rose-pine/neovim",
+		name = "rose-pine",
+	},
 })
-
-require("vague").setup({
-  transparent = false,
-  bold = true,
-  italic = true,
-})
-
-vim.cmd("colorscheme vague")
+require("rose-pine").setup()
+vim.cmd("colorscheme rose-pine")
